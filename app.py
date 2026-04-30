633a6f16ac5b52e86f35142f0733b4d8b00b75e2
mkshamees <122865210+mkshamees@users.noreply.github.com>
2026-04-30 03:29:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -91,9 +91,9 @@ if page == "Executive Overview":
 
     col1, col2, col3 = st.columns(3)
 
-   col1.metric("💰 Revenue", f"{daily_sales['TotalPrice'].sum():,.0f}", "+5%")
-col2.metric("📦 Orders", len(daily_sales), "+2%")
-col3.metric("👥 Customers", rfm.shape[0], "+3%")
+    col1.metric("💰 Revenue", f"{daily_sales['TotalPrice'].sum():,.0f}", "+5%")
+    col2.metric("📦 Orders", len(daily_sales), "+2%")
+    col3.metric("👥 Customers", rfm.shape[0], "+3%")
 
     st.markdown("### Revenue Trend")
 
